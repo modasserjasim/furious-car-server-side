05226752892175bc167d496f24e6b586e754be9f
Modasserul Hoque <modasserulhaq.jasim@gmail.com>
2022-11-04 02:59:21 +0600

    basic node mongo setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 4200;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqm0pbr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+app.get('/', (req, res) => {
+    res.send('Furious car server is running');
+})
+
+app.listen(port, () => {
+    console.log(`Furious car server running on port ${port}`);
+})
